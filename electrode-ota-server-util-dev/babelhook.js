37ed66e0885d3df5dfdc4baf2715384f4e31f74e
Justin Spears <jspears@walmartlabs.com>
2017-04-09 18:03:46 -0700

    moved tests

---
+++ electrode-ota-server-util-dev/babelhook.js
@@ -0,0 +1,70 @@
+"use strict";
+
+require('babel-polyfill');
+const conf = require('./babelrc.dev.json');
+const Module = require('module');
+const path = require('path');
+const babelRegister = require('babel-register');
+const oload = Module._load;
+
+if (process.env.COVERAGE) {
+    console.log('Has Coverage');
+    conf.plugins.push([
+        "istanbul",
+        {
+            "exclude": [
+                "**/test/*-test.js"
+            ]
+        }
+    ]);
+}
+const project = path.join(__dirname, '..');
+
+//only look into ern- projects that have a src directory.
+conf.only = /electrode-ota-server-[^/]*\/(src|test|lib)/;
+
+/**
+ * Babelify all ern- projects.  And if they
+ * are an ern- project than use the src.
+ *
+ * @param file
+ * @param parent
+ * @private
+ */
+function normalizePath(file, parent) {
+    if (/^\./.test(file)) {
+        return file;
+    }
+    //fixes issue with globa-cli calling local-cli.
+    if (/\/ern-local-cli$/.test(file)) {
+        return 'ern-local-cli'
+    }
+    if (/^\//.test(file)) {
+        if (file.startsWith(project + '/electrode-ota-server-')) {
+            return file.replace(project + '/', '');
+        }
+        return file;
+    }
+    if (/(@walmart\/)?ern-/.test(file)) {
+        return file.replace('@walmart/', '');
+    }
+    return;
+}
+Module._load = function (file, parent) {
+
+    let absFile = normalizePath(file, parent);
+    if (absFile) {
+
+
+        let parts = absFile.split('/');
+        let scope = parts[0], pkg = parts[1], rest = parts.slice(2).join(path.sep);
+        if (/ern-/.test(scope)) {
+            if (!pkg || pkg == 'dist') pkg = 'src';
+            file = path.join(project, scope, pkg, rest || 'index');// `${project}/${pkg}/${rest ? '/' + rest : ''}`
+        }
+    }
+
+    return oload(file, parent);
+};
+babelRegister(conf);
+
