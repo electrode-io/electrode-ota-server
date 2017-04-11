cd85e77856fada600863918984f5df812fa167e5
Justin Spears <jspears@walmartlabs.com>
2017-04-10 17:06:11 -0700

    adds coverage - Tests Pass 88.78%

--- electrode-ota-server-util-dev/bin/ota-nyc.js
+++ electrode-ota-server-util-dev/bin/ota-nyc.js
@@ -6,3 +6,4 @@ if (!fs.existsSync('test')) {
 }
 process.argv.push('--sourceMap=false', '--reportDir=.coverage', '--instrument=false', '--all', '--include=src/**/*.js', 'mocha', '--compilers', `js:${__dirname}/../babelhook-coverage`, 'test/*-test.js');
 require('nyc/bin/nyc');
+//--sourceMap=false --reportDir=.coverage --instrument=false --all --include=src/**/*.js mocha --compilers js:./electrode-ota-server-dev-util/babelhook-coverage'
