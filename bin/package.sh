16301410edfd3eaa8e09caa943260f8df004b1f4
Justin Spears <jspears@walmartlabs.com>
2017-04-14 12:08:29 -0700

    small improvment to package.sh

--- bin/package.sh
+++ bin/package.sh
@@ -9,18 +9,26 @@ valid_name() {
   [ -d "$1" ] && help "directory already exists $1"
    echo $1 | grep -q '^electrode-ota-server'
 }
+version(){
+    [ -n "$1" ] && echo $1
+    node -p "require('./lerna.json').version"
+}
 
 PKG=$1
 valid_name $PKG
 
-mkdir $PKG
+VERSION=${VERSION:-$(version $2)}
+SHORT_NAME=${PKG/electrode-ota-server-/}
+
+[ ! -d $PKG ] && mkdir $PKG
 cd $PKG
 
+if [ ! -f package.json ]; then
 (
 cat <<EOF
 {
   "name": "${PKG}",
-  "version": "1.0.0",
+  "version": "${VERSION}",
   "main": "lib/index.js",
   "scripts": {
     "test": "ota-mocha",
@@ -29,18 +37,36 @@ cat <<EOF
     "prepublish": "npm run build"
   },
   "dependencies": {
-    "electrode-ota-server-diregister":"1.0.0",
-    "electrode-ota-server-util":"1.0.0"
+    "electrode-ota-server-diregister":"${VERSION}",
+    "electrode-ota-server-util":"${VERSION}"
   },
   "devDependencies": {
-    "electrode-ota-server-util-dev":"1.0.0"
+    "electrode-ota-server-util-dev":"${VERSION}"
   }
 }
 EOF
 )>package.json
+fi
 
-mkdir src
-mkdir test
+[ ! -d src ] && mkdir src
+[ ! -d test ] && mkdir test
 touch src/index.js
+
+if [ ! -f test/${PKG}.json ]; then
+(
+cat <<EOF
+import plugin from '../lib/index';
+import {expect} from 'chai';
+
+describe('${PKG}', function(){
+
+  it('should do something');
+
+});
+
+
+EOF
+)>test/${SHORT_NAME}.json
+fi
 
 git add ../$PKG
