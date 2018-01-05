32a2dd09e6ff96cd9a21f39e760413e2c5fdb774
Dat Vong <Dat.Vong@walmartlabs.com>
2018-01-05 11:45:32 -0800

    prettier-ify another file

--- electrode-ota-server-test-support/src/init-maria-dao.js
+++ electrode-ota-server-test-support/src/init-maria-dao.js
@@ -1,35 +1,40 @@
-import {daoDriver, createDatabaseForTest} from 'electrode-ota-server-dao-mariadb';
-import {daoFactory} from 'electrode-ota-server-dao-factory';
+import {
+  daoDriver,
+  createDatabaseForTest
+} from "electrode-ota-server-dao-mariadb";
+import { daoFactory } from "electrode-ota-server-dao-factory";
 
 let client;
-const mockRegister = (driver, options, callback) => { callback() };
+const mockRegister = (driver, options, callback) => {
+  callback();
+};
 const mockLogger = { info: () => {}, error: () => {} };
 
 export const shutdownMaria = async () => {
-    if (client) {
-        await client.closeAsync();
-        client = null;
-    }
+  if (client) {
+    await client.closeAsync();
+    client = null;
+  }
 };
 
 export default async (options = {}) => {
-    try {
-        if (client != null) {
-            throw new Error('shutdown was not called');
-        }
-        const config = {
-            "host": "localhost",
-            "port": 3306,
-            "db": "ota_db_test",
-            "user": "root",
-            "password":""
-        };
-        await createDatabaseForTest(config);
-
-        const driver = await daoDriver(config, mockLogger);
-        return daoFactory({}, driver, mockLogger);
-    } catch(e) {
-        console.trace(e);
-        throw e;
+  try {
+    if (client != null) {
+      throw new Error("shutdown was not called");
     }
-}
+    const config = {
+      host: "localhost",
+      port: 3306,
+      db: "ota_db_test",
+      user: "root",
+      password: ""
+    };
+    await createDatabaseForTest(config);
+
+    const driver = await daoDriver(config, mockLogger);
+    return daoFactory({}, driver, mockLogger);
+  } catch (e) {
+    console.trace(e);
+    throw e;
+  }
+};
