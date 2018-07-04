6e8602fe2520e83f0f7867ccd9d58bd4c199f050
Dat Vong <Dat.Vong@walmartlabs.com>
2018-07-03 17:20:12 -0700

    reset UI on login

--- electrode-ota-ui/src/pages/AppPage/reducers.js
+++ electrode-ota-ui/src/pages/AppPage/reducers.js
@@ -1,36 +1,50 @@
-import {RECEIVE_APP, TRANSFERED_APP, RENAMED_APP} from './actions';
-import {ADDED_COLLABORATOR, REMOVED_COLLABORATOR} from '../Collaborator/actions';
-import {ADDED_DEPLOYMENT, RENAMED_DEPLOYMENT, REMOVED_DEPLOYMENT} from '../Deployment/actions';
+import { RECEIVE_APP, TRANSFERED_APP, RENAMED_APP } from "./actions";
+import {
+	ADDED_COLLABORATOR,
+	REMOVED_COLLABORATOR
+} from "../Collaborator/actions";
+import {
+	ADDED_DEPLOYMENT,
+	RENAMED_DEPLOYMENT,
+	REMOVED_DEPLOYMENT
+} from "../Deployment/actions";
+import { RESET } from "../Shell/actions";
 
-export const selectedApp = (state = {}, {type, value, isError, error})=> {
-    switch (type) {
-        case RECEIVE_APP:
-            if (isError) {
-                return {
-                    ...state,
-                    isFetching: false,
-                    error
-                }
-            }
+export const selectedApp = (state = {}, { type, value, isError, error }) => {
+	switch (type) {
+		case RECEIVE_APP:
+			if (isError) {
+				return {
+					...state,
+					isFetching: false,
+					error
+				};
+			}
 
-            return {isStale: false, isError: false, isFetching: false, ...value};
-        case ADDED_COLLABORATOR:
-        case REMOVED_COLLABORATOR:
-        case ADDED_DEPLOYMENT:
-        case REMOVED_DEPLOYMENT:
-        case RENAMED_DEPLOYMENT:
-        case TRANSFERED_APP:
-        case RENAMED_APP:
-            if (isError) {
-                return {
-                    ...state,
-                    isFetching: false
-                }
-            }
-            return {...state, isFetching: false, isError: false, isStale: Date.now()};
-
-        default:
-            return state;
-    }
+			return { isStale: false, isError: false, isFetching: false, ...value };
+		case ADDED_COLLABORATOR:
+		case REMOVED_COLLABORATOR:
+		case ADDED_DEPLOYMENT:
+		case REMOVED_DEPLOYMENT:
+		case RENAMED_DEPLOYMENT:
+		case TRANSFERED_APP:
+		case RENAMED_APP:
+			if (isError) {
+				return {
+					...state,
+					isFetching: false
+				};
+			}
+			return {
+				...state,
+				isFetching: false,
+				isError: false,
+				isStale: Date.now()
+			};
+		case RESET:
+			return {};
+		default:
+			return state;
+	}
 };
-export default ({selectedApp})
+export default { selectedApp };
--- electrode-ota-ui/src/pages/Login/index.js
+++ electrode-ota-ui/src/pages/Login/index.js
@@ -1,39 +1,45 @@
-import Login from './Login';
-import {connect} from 'react-redux';
-import boundDispatched from '../../util/boundDispatched';
-import {login} from './actions';
-import {navigate} from '../Shell/actions';
+import Login from "./Login";
+import { connect } from "react-redux";
+import boundDispatched from "../../util/boundDispatched";
+import { login } from "./actions";
+import { navigate, loginReset } from "../Shell/actions";
 
-const mapStateToProps = ({authorization:{host, token, isError, error, hostname, hideHost}}, props)=> {
-	return {value: {host, token, hostname, hideHost}, isError: (isError || error != null), error};
+const mapStateToProps = (
+	{ authorization: { host, token, isError, error, hostname, hideHost } },
+	props
+) => {
+	return {
+		value: { host, token, hostname, hideHost },
+		isError: isError || error != null,
+		error
+	};
 };
 
-const mapDispatchToProps = (dispatch, {location = {}})=>({
-	onSubmit({host, token, remember} = {}){
+const mapDispatchToProps = (dispatch, { location = {} }) => ({
+	onSubmit({ host, token, remember } = {}) {
 		if (!(host && token)) {
 			return;
 		}
-		dispatch(login(host, token)).then(({value})=> {
+		dispatch(login(host, token)).then(({ value }) => {
 			if (!value) {
 				return value;
 			}
 			if (remember) {
-				sessionStorage.token = token || '';
-				sessionStorage.host = host || '';
+				sessionStorage.token = token || "";
+				sessionStorage.host = host || "";
 			}
+			dispatch(loginReset());
 			if (location.state && location.state.nextPathname) {
-				dispatch(navigate(location.state.nextPathname))
+				dispatch(navigate(location.state.nextPathname));
 			} else {
-				dispatch(navigate('/'));
+				dispatch(navigate("/"));
 			}
 		});
 	}
 });
 
-
-export default connect(mapStateToProps, mapDispatchToProps, boundDispatched)(Login);
-
-
-
-
-
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+	boundDispatched
+)(Login);
