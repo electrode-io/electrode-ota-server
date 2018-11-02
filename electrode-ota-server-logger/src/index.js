d7d771b3be8488198006d12b904ce6669e65b028
Dat Vong <Dat.Vong@walmartlabs.com>
2018-11-02 10:04:53 -0700

    Add ability to configure the log level. (#83)
    * Add ability to specify log level
    
    * remove dependency

--- electrode-ota-server-logger/src/index.js
+++ electrode-ota-server-logger/src/index.js
@@ -1,25 +1,37 @@
 import diregister from "electrode-ota-server-diregister";
 
+const levels = ["error", "warn", "info", "log", "debug", "trace"];
+export function noOp() {}
+
 // default logger - bunyan (https://github.com/trentm/node-bunyan)
 export function loggerFactory(options, register) {
-
-    return ["error", "warn", "info", "log", "debug", "trace"].reduce((logger, method) => {
-        logger[method] = function(fields, shortMsg, longMsg) {
-            try {
-                console[method](fields, shortMsg, longMsg || "");
-            } catch (e) {
-                console.error("error while logging", e);
-            }
+  let loggers = {};
+  let noOpIdx = levels.length;
+  if (options && options.level) {
+    noOpIdx = levels.indexOf(options.level.toLowerCase()) + 1;
+  }
+  return levels.reduce((logger, method, idx) => {
+    if (idx >= noOpIdx) {
+      logger[method] = noOp;
+    } else {
+      logger[method] = function(fields, shortMsg, longMsg) {
+        try {
+          console[method](fields, shortMsg, longMsg || "");
+        } catch (e) {
+          console.error("error while logging", e);
         }
-        return logger;
-    }, {});
-
+      };
+    }
+    return logger;
+  }, {});
 }
 
-
-export const register = diregister({
-    name: 'ota!logger',
+export const register = diregister(
+  {
+    name: "ota!logger",
     multiple: false,
     connections: false,
-    dependencies : ["electrode:register"]
-}, loggerFactory);
+    dependencies: ["electrode:register"]
+  },
+  loggerFactory
+);
