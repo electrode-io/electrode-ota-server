b9cbacce5dc1e3067a819f1ad8c50cab7ec2c8eb
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2020-04-08 17:21:57 -0700

    cleanup CCM inputs

--- electrode-ota-server-ccm/src/index.js
+++ electrode-ota-server-ccm/src/index.js
@@ -1,20 +1,10 @@
 import diregister from "electrode-ota-server-diregister";
 import CCMStore from "@walmart/electrode-ccm-store";
 
-let host = "http://scm.prod.walmart.com";
-if (process.env.NODE_ENV !== "production") {
-    host = "http://scm.stg.walmart.com";
-}
-
 const options = {
-    host,
-    cloudEnv: process.env.ONEOPS_ENVIRONMENT || "stg-dfw2",
     ccmKey: "absetup",
-    env: process.env.ONEOPS_ENVPROFILE || "QA",
     serviceName: "electrode-ota"
 };
-options.ccmKey = "features";
-options.serviceName = "home-app";
 const ccm = new CCMStore(options);
 
 // method used by external modules to retrieve the ccm config
