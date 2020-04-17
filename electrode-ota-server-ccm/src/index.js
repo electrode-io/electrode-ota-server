10438058d26fbaa453eb32801f4305febefd61af
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2020-04-17 15:37:56 -0700

    removing dao deps for ota-ccm

--- electrode-ota-server-ccm/src/index.js
+++ electrode-ota-server-ccm/src/index.js
@@ -1,15 +1,15 @@
 import diregister from "electrode-ota-server-diregister";
 
 // method used by external modules to retrieve the ccm config
-export const getConfig = (options, dao) => {
-    // getConfig is not yet present within electrode-ota-server
-    //  A module has to be passed into to override it or has to be implemented in app-dao
-    return key => typeof dao.getConfig === "function" ? dao.getConfig(key) : undefined;
+export const getConfig = (/*options*/) => {
+    // this method should be overrided by custom moduel that fetches configs from cloud
+    //  by default, if not overriden return 'false'
+    return (/*key*/) => false;
 };
 
 export const register = diregister({
     name: "ota!ccm",
     multiple: false,
     connections: false,
-    dependencies: ["ota!dao"]
+    dependencies: []
 }, getConfig);
