85edc0ec01260d6f05240394d9b865c9df04ace5
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2020-04-17 18:20:28 -0700

    ccm::getConfig returns false if not overriden

--- electrode-ota-server-ccm/src/index.js
+++ electrode-ota-server-ccm/src/index.js
@@ -1,15 +1,14 @@
 import diregister from "electrode-ota-server-diregister";
 
-// method used by external modules to retrieve the ccm config
-export const getConfig = (options, dao) => {
-    // getConfig is not yet present within electrode-ota-server
-    //  A module has to be passed into to override it or has to be implemented in app-dao
-    return key => typeof dao.getConfig === "function" ? dao.getConfig(key) : undefined;
-};
-
+// An Abstract method (meant to be overriden)
+//  to get any configuration required
+export const getConfig = (/*options*/) => {
+    // if not overriden, by default return 'false'
+    return (/*key*/) => false;
+}
 export const register = diregister({
     name: "ota!ccm",
     multiple: false,
     connections: false,
-    dependencies: ["ota!dao"]
+    dependencies: []
 }, getConfig);
