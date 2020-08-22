56469e76ddcae2a26a8ebdfd68218e74fe157e8b
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2020-08-21 17:07:06 -0700

    added comments

--- electrode-ota-server-routes-acquisition/src/handlers.js
+++ electrode-ota-server-routes-acquisition/src/handlers.js
@@ -6,6 +6,7 @@ import * as abTest from "./ab-test";
 import isProtected from "./is-protected";
 
 const HTTP_OK = 200;
+const HTTP_UNAUTHORIZED = 401;
 const ok = reply => e => {
     if (e) return reply(e);
     reply("OK").code(HTTP_OK);
@@ -18,11 +19,15 @@ const handles = {
         const snakeCaseRequested = /update_check/.test(action);
         // if snake_case request? convert to camelCase
         const qs = snakeCaseRequested ? keysToCamelOrSnake(request.query) : request.query;
+        // retrieve the protected packages list
         const packsProtected = ccm("packsProtected");
-        const isRequestDisabled = isProtected(qs.deploymentKey, packsProtected);
-        if (/auth\/updateCheck/.test(action) && isRequestDisabled) {
-            return reply().code(401);
+        const isProtectedPack = isProtected(qs.deploymentKey, packsProtected);
+        // check if the request is through authenticated api?
+        //  if so, check if the requested package is protected?
+        if (/auth\/updateCheck/.test(action) && isProtectedPack) {
+            return reply().code(HTTP_UNAUTHORIZED);
         }
+        // invoke acquisition model updateCheck
         acquisitionUpdateCheck(qs, (e, updatedInfo) => {
             if (e) {
                 console.log("error making update check ", request.query, e.message);
--- electrode-ota-server-routes-acquisition/src/is-protected.js
+++ electrode-ota-server-routes-acquisition/src/is-protected.js
@@ -1,9 +1,10 @@
+// check if incoming deployment key is in the protected packages list
 const isProtected = (key, protectedPacks) => {
     if (key && protectedPacks && typeof protectedPacks === "string") {
         const packs = protectedPacks.split(",");
         return packs && packs.includes(key);
     }
-
+    // by default it's off the list
     return false;
 };
 
